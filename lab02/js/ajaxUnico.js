73b4215d2a533b79bcbf57187888d2c48443f57f
Gonzalo Neira <ggneiracc68@gmail.com>
2023-05-07 08:24:23 -0500

    v11 modifique archivo ajax

--- lab02/js/ajaxUnico.js
+++ lab02/js/ajaxUnico.js
@@ -66,10 +66,19 @@ function ver(variables,etiqueta,id,procesador){
 	if(objXHR){
 		//var host = document.getElementById('txtHost').value;
 		//var directorio = document.getElementById('txtDirectorio').value;
+/*
+		url: "http://urlprovedor.mx/ws/direcciones.asmx/getCarburaciones",
+		type: 'GET',
+		crossDomain: true,
+		dataType: 'jsonp',
+		success: function() { alert("Success"); },
+		error: function() { alert('Failed!'); },
+		beforeSend: setHeader
+*/
 
-		host="localhost";
-		directorio="clones/tienda01";
-		url="http://"+host+"/"+directorio+"/"+procesador;
+		host="elizabethzqr.github.io/";
+		directorio="tienda01";
+		url="https://"+host+"/"+directorio+"/"+procesador;
 
 		var losDatos = crearCadena(variables);			
 		objXHR.onreadystatechange=procesaRespuesta;
